8e954851d2a73864fb6f4e3e41696f39432aae54
Marat Metoff <marat.metoff@gmail.com>
2017-08-11 03:38:36 -0400

    Push Google

--- mmm/src/test/java/mmm/Regression.java
+++ mmm/src/test/java/mmm/Regression.java
@@ -1,20 +1,36 @@
 package mmm;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Regression {
-  
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/Marat Metoff/Documents/Libraries/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.com");
+	}
+	
 	@Test(groups = {"Regression"})
 	public void f() {
 		
 		System.out.println("Regression");
 		
 	}
-	
+
 	@Test
 	public void f1() {
-		
+
 		System.out.println("Functional");
-		
+
 	}
 }
